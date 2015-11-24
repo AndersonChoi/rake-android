06572c804dec9644ac00cddca452c1adf0ed43be
1ambda <1amb4a@gmail.com>
2015-11-24 18:59:06 +0900

    Ignore Token.java

--- testApp/src/main/java/com/skplanet/rake/application/Token.java
+++
@@ -1,9 +0,0 @@
-package com.skplanet.rake.application;
-
-import android.app.Application;
-
-public class Token extends Application {
-    // metric
-    public static String LIVE_TOKEN = "";
-    public static String DEV_TOKEN = "";
-}
