8e96d36402e12399cb9def14cdddfb52cab00827
Uzys <uzysjung@gmail.com>
2016-05-25 15:04:15 +0900

    RakeConfig 외부 노출 없앰.

--- rake-iOS-Library/Rake/RakeConfig.h
+++ rake-iOS-Library/Rake/RakeConfig.h
@@ -2,13 +2,13 @@
 #define RAKE_LIB_VERSION @"r0.5.0_c1.8.0"
 #define RAKE_LIB_BUILD_DATE @"2015-12-15 15:16:38"
 #define BUILD_BRANCH @"master"
-#define METRIC_TOKEN_DEV @"df234e764a5e4c3beaa7831d5b8ad353149495ac"
-#define METRIC_TOKEN_LIVE @"d839ca7a5875c8ac304d465bfcfd78d95b4726eb"
-#define DEV_SERVER_URL @"https://pg.rake.skplanet.com:8443/log"
-#define LIVE_SERVER_URL @"https://rake.skplanet.com:8443/log/"
+#define METRIC_TOKEN_DEV @""
+#define METRIC_TOKEN_LIVE @""
+#define DEV_SERVER_URL @""
+#define LIVE_SERVER_URL @""
 
-#define CRASHLOGGER_TOKEN_DEV @"34e03e87dfd7152596e15bdefa4626de9bdf8c32"
-#define CRASHLOGGER_TOKEN_LIVE @"617b4dfd0ffd25fbb6740e4b5c6e2561c241e44"
+#define CRASHLOGGER_TOKEN_DEV @""
+#define CRASHLOGGER_TOKEN_LIVE @""
 
 // if you want to add crashreport you have to combine PLCarshReporter static lib with Rake static lib please check TARGET->Framework->Build Phase->script
 // 회사 사정으로 CRASHReporter 제거.
