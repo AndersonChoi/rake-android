ace299081047388889130201d64eef8be43e4a8a
Uzys <uzysjung@gmail.com>
2016-06-07 18:02:26 +0900

    base64 Licenst comment added

--- rake-iOS-Library/Rake/Base64.m
+++ rake-iOS-Library/Rake/Base64.m
@@ -2,8 +2,15 @@
 //  Base64.m
 //  rake-iOS-Library
 //
-//  Created by 1002125 on 2016. 1. 21..
+//  Created by Matt Gallagher on 2009/06/03.
+//  Modified by JungHyun Kim on 2014/04/10
+//  Copyright 2009 Matt Gallagher. All rights reserved.
 //
+//  Permission is given to use this source code file, free of charge, in any
+//  project, commercial or otherwise, entirely at your risk, with the condition
+//  that any redistribution (in part or whole) of source code must retain
+//  this copyright and permission notice. Attribution in compiled projects is
+//  appreciated but not required.
 //
 #if ! __has_feature(objc_arc)
 #error This file must be compiled with ARC. Either turn on ARC for the project or use -fobjc-arc flag on this file.
