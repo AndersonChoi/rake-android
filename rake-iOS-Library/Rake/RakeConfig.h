13c80c46b68d227f051751927ab8c22fc54df91a
Uzys <uzysjung@gmail.com>
2016-07-19 15:07:06 +0900

    RakeConfig은 토큰과 URL을 포함하고 있어서 공개하지 않습니다.

--- rake-iOS-Library/Rake/RakeConfig.h
+++ rake-iOS-Library/Rake/RakeConfig.h
@@ -1,7 +1,7 @@
-/* 아래의 변수들은 컴파일 시작 전 빌드타임에 치환되는 변수들로 변경시 gulpfile.js 도 변경해야 함*/
-#define RAKE_LIB_VERSION @"r0.5.0_c1.8.0"
-#define RAKE_LIB_BUILD_DATE @"2015-12-15 15:16:38"
-#define BUILD_BRANCH @"master"
+// 키를 포함하고 있어서 공개하지 않습니다.
+#define RAKE_LIB_VERSION @""
+#define RAKE_LIB_BUILD_DATE @""
+#define BUILD_BRANCH @""
 #define METRIC_TOKEN_DEV @""
 #define METRIC_TOKEN_LIVE @""
 #define DEV_SERVER_URL @""
