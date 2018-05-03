05f259b1d85d474f7c9287dc0969429f5f27893a
Hyeyoung Song <hyeyoung.song@sk.com>
2018-05-03 16:56:52 +0900

    v.0.5.1 릴리즈 준비

--- rake/src/main/java/com/rake/android/rkmetrics/config/RakeConfig.java
+++ rake/src/main/java/com/rake/android/rkmetrics/config/RakeConfig.java
@@ -12,5 +12,5 @@ public final class RakeConfig {
 
     // TODO: remove r0.5.0_c. it requires to modify server dep.
     // version number will be replaced automatically before compiling.
-    public static final String RAKE_LIB_VERSION = "r0.5.0_c0.5.0";
+    public static final String RAKE_LIB_VERSION = "r0.5.0_c0.5.1";
 }
